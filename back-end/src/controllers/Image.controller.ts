79c6b701711d7a800c3cd16903564cb42554a0d2
Cozmu <www.jorgereis@gmail.com>
2023-05-19 15:51:07 -0300

    fix: corrigindo rota na requisição da imagem

--- back-end/src/controllers/Image.controller.ts
+++ back-end/src/controllers/Image.controller.ts
@@ -4,10 +4,7 @@ import path from 'path'
 class ImageController {
   async getImage(req:Request, res:Response):Promise<void> {
     const { url } = req.params;
-    const imagePath = path.resolve(__dirname, '../../imgs', url);
-    console.log(imagePath);
-
-    return res.sendFile(imagePath);
+    return res.sendFile(path.resolve(`imgs/${url}`));
   }
 }
 
