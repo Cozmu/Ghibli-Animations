50d53109617e98f343378d16468613acbcc928d3
Cozmu <www.jorgereis@gmail.com>
2023-05-18 22:38:27 -0300

    chore: tentando resolver bug de rota de arquvio

--- back-end/src/controllers/Image.controller.ts
+++ back-end/src/controllers/Image.controller.ts
@@ -4,7 +4,10 @@ import path from 'path'
 class ImageController {
   async getImage(req:Request, res:Response):Promise<void> {
     const { url } = req.params;
-    return res.sendFile(path.resolve(__dirname, `../../img/${url}`));
+    const imagePath = path.resolve(__dirname, '../../imgs', url);
+    console.log(imagePath);
+
+    return res.sendFile(imagePath);
   }
 }
 
